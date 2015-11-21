6b3edf5db3e61239d5c6479d28cd2483b4f86074
Damien Klinnert <mail@damienklinnert.de>
2015-11-22 08:04:54 +1100

    Add Pact querystring checks

--- src/Pact.hs
+++ src/Pact.hs
@@ -2,7 +2,11 @@
 module Pact where
 
 import Data.Char (toUpper)
-import qualified Data.ByteString.Lazy as B
+import qualified Data.List as L
+import qualified Data.ByteString.Char8 as CS
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+import qualified Network.HTTP.Types as H
 import Data.Aeson
 
 data TestCase = TestCase
@@ -17,7 +21,7 @@ instance FromJSON TestCase where
 
 checkCase :: String -> IO ()
 checkCase file = do
-  methodDifferent <- B.readFile file
+  methodDifferent <- BL.readFile file
   let (Just x) = decode methodDifferent :: Maybe TestCase
   let exp = (expected x)
   let act = (actual x)
@@ -47,20 +51,34 @@ tests =
  , "/path/matches.json"
  , "/path/missing trailing slash in path.json"
  , "/path/unexpected trailing slash in path.json"
+ , "/query/different order.json"
+ , "/query/different params.json"
+ , "/query/matches.json"
+ , "/query/missing params.json"
+ , "/query/same parameter different values.json"
+ , "/query/same parameter multiple times in different order.json"
+ , "/query/same parameter multiple times.json"
+ , "/query/trailing ampersand.json"
+ , "/query/unexpected param.json"
  ]
 
 main :: IO ()
 main = mapM_ (checkCase . (testBase ++)) tests
 
 verifyRequest :: RequestDesc -> RequestDesc -> Diff
-verifyRequest expected actual = foldl1 (&&) [
-    verifyMethod (method expected) (method actual),
-    verifyPath (path expected) (path actual)
-  ]
+verifyRequest expected actual = foldl1 (&&)
+ [ verifyMethod (method expected) (method actual)
+ , verifyPath (path expected) (path actual)
+ , verifyQuery (query expected) (query actual)
+ ]
 
 verifyMethod :: String -> String -> Diff
 verifyMethod expected actual = uppercase expected == uppercase actual
   where uppercase = map toUpper
 
 verifyPath :: String -> String -> Diff
-verifyPath expected actual = expected == actual
+verifyPath = (==)
+
+verifyQuery :: String -> String -> Diff
+verifyQuery expected actual = toQ expected == toQ actual
+  where toQ s = L.sortOn fst $ H.parseSimpleQuery (CS.pack s)
