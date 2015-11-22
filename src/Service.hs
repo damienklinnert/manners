75cd0c1bd21f6f03ce29cf351b3071c52759ddc6
Damien Klinnert <mail@damienklinnert.de>
2015-11-22 21:49:40 +1100

    Add first spike of fake provider unmarshalling

--- src/Service.hs
+++ src/Service.hs
@@ -5,7 +5,9 @@ module Service
  ) where
 
 import qualified Data.List as L
+import qualified Data.HashMap.Strict as HM
 import qualified Control.Concurrent.MVar as M
+import Data.Aeson as Aeson
 import qualified Network.Wai as W
 import qualified Network.HTTP.Types as H
 import qualified Network.Wai.Handler.Warp as Warp
@@ -24,11 +26,18 @@ providerService :: W.Application
 providerService request respond =
   case route of
 
-    ("POST", ["interactions"], True) -> respond $
-      W.responseLBS H.status200 [("Content-Type", "text/plain")] "Adds a new interaction"
+    ("POST", ["interactions"], True) -> do
+      body <- W.strictRequestBody request
+      let (Just interaction) = Aeson.decode body :: Maybe Pact.Interaction
+      putStrLn (show interaction)
+      respond $ W.responseLBS H.status200 [("Content-Type", "text/plain")] "Add interaction"
 
-    ("PUT", ["interactions"], True) -> respond $
-      W.responseLBS H.status200 [("Content-Type", "text/plain")] "Sets all interactions"
+    ("PUT", ["interactions"], True) -> do
+      body <- W.strictRequestBody request
+      let (Just interactionWrapper) = Aeson.decode body :: Maybe InteractionWrapper
+      let interactions = wrapperInteractions interactionWrapper
+      putStrLn (show interactions)
+      respond $ W.responseLBS H.status200 [("Content-Type", "text/plain")] "Sets all interactions"
 
     ("DELETE", ["interactions"], True) -> respond $
       W.responseLBS H.status200 [("Content-Type", "text/plain")] "Delete registered interactions"
@@ -36,8 +45,11 @@ providerService request respond =
     ("GET", ["interactions", "verification"], True) -> respond $
       W.responseLBS H.status200 [("Content-Type", "text/plain")] "Verify set-up interactions"
 
-    ("POST", ["pact"], True) -> respond $
-      W.responseLBS H.status200 [("Content-Type", "text/plain")] "Persist verified interactions as contract"
+    ("POST", ["pact"], True) -> do
+      body <- W.strictRequestBody request
+      let (Just contactDesc) = Aeson.decode body :: Maybe ContractDescription
+      putStrLn (show contactDesc)
+      respond $ W.responseLBS H.status200 [("Content-Type", "text/plain")] "Persist verified interactions as contract"
 
     _ -> respond $ W.responseLBS H.status200 [("Content-Type", "text/plain")] "Default Handler"
 
@@ -47,3 +59,19 @@ providerService request respond =
             of (Just _) -> True
                _ -> False
         hasAdminHeader (h, v) = h == "X-Pact-Mock-Service" && v == "True"
+
+
+data InteractionWrapper = InteractionWrapper { wrapperInteractions :: [Pact.Interaction] } deriving (Show)
+instance Aeson.FromJSON InteractionWrapper where
+  parseJSON (Object v) = InteractionWrapper <$> v .: "interactions"
+
+data ServiceDescription = ServiceDescription { serviceName :: String } deriving (Show)
+instance Aeson.FromJSON ServiceDescription where
+  parseJSON (Object v) = ServiceDescription <$> v .: "name"
+
+data ContractDescription = ContractDescription
+ { contractConsumer :: ServiceDescription
+ , contractProvider :: ServiceDescription
+ } deriving (Show)
+instance Aeson.FromJSON ContractDescription where
+  parseJSON (Object v) = ContractDescription <$> v .: "consumer" <*> v .: "provider"
