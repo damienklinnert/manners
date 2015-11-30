b7eb834eecbdcacc9b854233915491a4971341eb
Damien Klinnert <dklinnert@atlassian.com>
2015-12-01 09:56:35 +1100

    Unpack manners executable after installing manners-bin

--- clients/js/manners-bin/support/install.js
+++ clients/js/manners-bin/support/install.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var mannersBin = require('../src/manners.js');
+var spawn = require('child_process').spawn;
 
 mannersBin.run(['--version'], function (err) {
   if (err) {
     return console.error(err);
   }
 
-  console.log('manners installed');
+  console.log('manners installed, unpacking');
+  spawn('gzexe', ['-d', mannersBin.path()], { stdio: 'inherit' })
+  .on('exit', function () {
+    console.log('manners unpacked, ready');
+  });
+
 });
