b9a78ff7d860e7c49d6fe8b970e9013f0c33688e
Sam Rijs <srijs@airpost.net>
2016-07-04 16:44:17 +1000

    fix manners-bin download for manners 0.5.0.0

--- clients/js/manners-bin/src/manners.js
+++ clients/js/manners-bin/src/manners.js
@@ -7,7 +7,7 @@ var BASE = 'https://github.com/mannersio/manners/releases/download/v0.5.0.0/';
 
 var filePathForPlatform = function (platform) {
   var p = { darwin: '-osx', linux: '-linux' }[platform];
-  return 'manners-0.4.1.0-x86_64' + p;
+  return 'manners-0.5.0.0-x86_64' + p;
 };
 
 module.exports = new BinWrapper()
