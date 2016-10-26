18584bded4a570238019a19915f68855bbf2185a
Jimmy Luong <jluong@atlassian.com>
2016-10-26 17:34:54 +1100

    output manners server to the console with debug flag or env variable.

--- clients/js/manners/index.js
+++ clients/js/manners/index.js
@@ -1,217 +1,265 @@
 'use strict';
 
-var spawn = require('child_process').spawn;
-var util = require('util');
-var http = require('http');
-var axios = require('axios');
-var mannersBin = require('manners-bin');
+const spawn = require('child_process').spawn;
+const util = require('util');
+const http = require('http');
+const axios = require('axios');
+const mannersBin = require('manners-bin');
 
-var ProviderError = module.exports.ProviderError = function (message) {
-  Error.call(this);
-  Error.captureStackTrace(this, ProviderError);
-  this.name = 'ProviderError';
-  this.message = message || '';
-};
+class ProviderError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message || '';
 
-ProviderError.prototype = Object.create(Error.prototype);
-ProviderError.prototype.constructor = ProviderError;
-
-var FakeProvider = module.exports.FakeProvider = function (opts) {
-  this._process = null;
-  this._opts = opts;
-};
-
-FakeProvider.prototype.start = function (done) {
-  return new Promise(function (resolve, reject) {
-    this._process = spawn(mannersBin, ['fake-provider']);
-    this._process.stdout.on('data', function (d) {
-      if (d.toString().indexOf('listen') !== -1) {
-        resolve();
-      }
-    });
-  }.bind(this));
-};
-
-FakeProvider.prototype.stop = function () {
-  return new Promise(function (resolve, reject) {
-    this._process.on('exit', resolve);
-    this._process.kill();
-  }.bind(this));
-};
-
-FakeProvider.prototype.interactions = function () {
-  return new InteractionGroup({
-    baseUrl: 'http://localhost:1234',
-    consumer: this._opts.consumer,
-    provider: this._opts.provider
-  });
-};
-
-var InteractionGroup = module.exports.InteractionGroup = function (cfg, interactions) {
-  this._cfg = cfg;
-  this._interactions = interactions || [];
-};
-
-InteractionGroup.prototype.add = function (interaction) {
-  return new InteractionGroup(this._cfg, this._interactions.concat(interaction));
-};
-
-InteractionGroup.prototype.setup = function (readyFn) {
-  var cfg = this._cfg;
-  var axiosCfg = {
-    timeout: 500,
-    headers: { 'X-Pact-Mock-Service': 'True' }
-  };
-  var rethrow = function (fn) {
-    return function (err) {
-      if (err instanceof ProviderError) { throw err; }
-      fn(err);
-    }
-  };
-  var cleanup = function () {
-    return axios.delete(cfg.baseUrl + '/interactions', axiosCfg);
-  };
-
-  return axios
-    .put(cfg.baseUrl + '/interactions', { interactions: this._interactions }, axiosCfg)
-    .catch(function (err) {
-      throw new ProviderError('Could not setup interactions');
-    })
-    .then(function () {
-      return readyFn();
-    })
-    .catch(rethrow(function (err) {
-      var msg = 'Promise returned from readyFn did not resolve:\n' + err;
-      throw new ProviderError(msg);
-    }))
-    .then(function () {
-      return axios.get(cfg.baseUrl + '/interactions/verification', axiosCfg);
-    })
-    .catch(rethrow(function (err) {
-      var msg = 'Verification failed with error:\n' + JSON.stringify(err.data.error, null, 2);
-      throw new ProviderError(msg);
-    }))
-    .then(
-      function () {
-        return cleanup();
-      }, function (err) {
-        return cleanup().then(function () {
-          throw err;
-        });
-      }
-    )
-    .catch(rethrow(function (err) {
-      throw new ProviderError('Cleaning up interactions failed');
-    }))
-    .then(function () {
-      var contract = {
-        consumer: { name: cfg.consumer },
-        provider: { name: cfg.provider }
-      };
-      return axios.post(cfg.baseUrl + '/pact', contract, axiosCfg);
-    })
-    .catch(rethrow(function (err) {
-      throw new ProviderError('Could not write contract:\n' + err);
-    }));
-};
-
-var FakeConsumer = module.exports.FakeConsumer = function (contractPath, providerUrl) {
-  this._cfg = {
-    contractPath: contractPath,
-    providerUrl: providerUrl
-  };
-};
-
-FakeConsumer.prototype.states = function () {
-  return new StateGroup(this._cfg, []);
-};
-
-var StateGroup = module.exports.StateGroup = function (cfg, states) {
-  this._cfg = cfg;
-  this._states = states;
+    Error.call(this);
+    Error.captureStackTrace(this, ProviderError);
+  }
 }
+exports.ProviderError = ProviderError;
 
-StateGroup.prototype.whenDefault = function (setupFn, teardownFn) {
-  var states = this._states.concat({ state: '__MANNERS_DEFAULT_STATE__', setupFn: setupFn, teardownFn: teardownFn });
-  return new StateGroup(this._cfg, states);
-};
-
-StateGroup.prototype.when = function (state, setupFn, teardownFn) {
-  var states = this._states.concat({ state: state, setupFn: setupFn, teardownFn: teardownFn });
-  return new StateGroup(this._cfg, states);
-};
-
-StateGroup.prototype.run = function () {
-  var service = new StateHelperService(this._states);
-
-  var spawnClient = function () {
-    return new Promise(function (resolve, reject) {
-      var process = spawn(mannersBin, ['fake-consumer', this._cfg.contractPath, this._cfg.providerUrl], { stdio: 'inherit' });
-      process.on('exit', function (statusCode) {
-        if (statusCode !== 0) { return reject(new Error('Failed to run fake-consumer')); }
-        resolve();
-      });
-      process.on('error', reject);
-    }.bind(this));
-  }.bind(this);
-
-  return service.start(2345)
-    .then(spawnClient)
-    .then(function () {
-      console.log('SUCCESS');
-      process.exit();
-    })
-    .catch(function (err) {
-      console.log('FAILURE', err);
-      process.exit(1);
-    });
-};
-
-var StateHelperService = function (states) {
-  this._stateMap = states.reduce(function (mem, cur) {
-    mem[cur.state] = cur;
-    return mem;
-  }, {});
-};
-
-StateHelperService.prototype.start = function (port) {
-  return new Promise(function (resolve, reject) {
-    http
-      .createServer(this._handleRequest.bind(this))
-      .listen(port, function (err) {
-        if (err) { return reject(err); }
-        resolve();
-      });
-  }.bind(this));
-};
-
-StateHelperService.prototype._handleRequest = function (req, res) {
-  if (req.method !== 'POST' && req.url !== '/state') {
-    res.statusCode = 500;
-    return res.end();
+class InteractionGroup {
+  constructor(cfg, interactions) {
+    this._cfg = cfg;
+    this._interactions = interactions || [];
   }
 
-  var data = '';
-  req.on('data', function (d) { data += d.toString(); });
-  req.on('end', function () {
-    var payload = JSON.parse(data);
-    var stateCfg = this._stateMap[payload.state];
+  add(interaction) {
+    return new InteractionGroup(this._cfg, this._interactions.concat(interaction));
+  }
 
-    if (!stateCfg) {
+  setup(readyFn) {
+    const cfg = this._cfg;
+    const axiosCfg = {
+      timeout: 500,
+      headers: {'X-Pact-Mock-Service': 'True'}
+    };
+    const rethrow = (fn) => {
+      return (err) => {
+        if (err instanceof ProviderError) {
+          throw err;
+        }
+        fn(err);
+      };
+    };
+    const cleanup = () => {
+      return axios.delete(cfg.baseUrl + '/interactions', axiosCfg);
+    };
+
+    return axios
+      .put(cfg.baseUrl + '/interactions', {interactions: this._interactions}, axiosCfg)
+      .catch((err) => {
+        throw new ProviderError('Could not setup interactions:' + err);
+      })
+      .then(() => {
+        return readyFn();
+      })
+      .catch(rethrow((err) => {
+        const msg = 'Promise returned from readyFn did not resolve: ' + err;
+        throw new ProviderError(msg);
+      }))
+      .then(() => {
+        return axios.get(cfg.baseUrl + '/interactions/verification', axiosCfg);
+      })
+      .catch(rethrow((err) => {
+        const msg = 'Verification failed with error: ' + JSON.stringify(err.data.error, null, 2);
+        throw new ProviderError(msg);
+      }))
+      .then(
+        () => {
+          return cleanup();
+        }, (err) => {
+          return cleanup().then(() => {
+            throw err;
+          });
+        }
+      )
+      .catch(rethrow((err) => {
+        throw new ProviderError('Cleaning up interactions failed: ' + err);
+      }))
+      .then(() => {
+        const contract = {
+          consumer: {name: cfg.consumer},
+          provider: {name: cfg.provider}
+        };
+        return axios.post(cfg.baseUrl + '/pact', contract, axiosCfg);
+      })
+      .catch(rethrow((err) => {
+        throw new ProviderError('Could not write contract: ' + err);
+      }));
+  }
+}
+exports.InteractionGroup = InteractionGroup;
+
+class StateHelperService {
+  constructor(states) {
+    this._stateMap = states.reduce((mem, cur) => {
+      mem[cur.state] = cur;
+      return mem;
+    }, {});
+  }
+
+  start(port) {
+    return new Promise((resolve, reject) => {
+      http
+        .createServer(this._handleRequest)
+        .listen(port, (err) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+    });
+  }
+
+  _handleRequest(req, res) {
+    if (req.method !== 'POST' && req.url !== '/state') {
       res.statusCode = 500;
-      res.end(JSON.stringify({ error: util.format('could not find state "%s" in config', payload.state)}));
-      return;
+      return res.end();
     }
 
-    var fnName = (payload.type === 'setup') ? 'setupFn' : 'teardownFn';
-    var stateFn = stateCfg[fnName] || function () {};
-    var promise = stateFn() || Promise.resolve();
-    promise.then(function () {
-      res.statusCode = 200;
-      res.end();
-    }, function () {
-      res.statusCode = 500;
-      res.end();
+    let data = '';
+    req.on('data', (d) => {
+      data += d.toString();
     });
-  }.bind(this));
-};
+    req.on('end', () => {
+      const payload = JSON.parse(data);
+      const stateCfg = this._stateMap[payload.state];
+
+      if (!stateCfg) {
+        res.statusCode = 500;
+        res.end(JSON.stringify({
+          error: util.format('could not find state "%s" in config', payload.state)
+        }));
+        return;
+      }
+
+      const fnName = (payload.type === 'setup') ? 'setupFn' : 'teardownFn';
+      const stateFn = stateCfg[fnName] || () => {
+        };
+      const promise = stateFn() || Promise.resolve();
+      promise.then(() => {
+        res.statusCode = 200;
+        res.end();
+      }, () => {
+        res.statusCode = 500;
+        res.end();
+      });
+    });
+  }
+}
+
+class StateGroup {
+  constructor(cfg, states) {
+    this._cfg = cfg;
+    this._states = states;
+  }
+
+  whenDefault(setupFn, teardownFn) {
+    const states = this._states.concat({
+      state: '__MANNERS_DEFAULT_STATE__',
+      setupFn: setupFn,
+      teardownFn: teardownFn
+    });
+    return new StateGroup(this._cfg, states);
+  }
+
+  when(state, setupFn, teardownFn) {
+    const states = this._states.concat({state: state, setupFn: setupFn, teardownFn: teardownFn});
+    return new StateGroup(this._cfg, states);
+  }
+
+  run() {
+    const service = new StateHelperService(this._states);
+
+    const spawnClient = () => {
+      return new Promise((resolve, reject) => {
+        const process = spawn(
+          mannersBin,
+          ['fake-consumer', this._cfg.contractPath, this._cfg.providerUrl],
+          {stdio: 'inherit'});
+
+        process.on('exit', (statusCode) => {
+          if (statusCode !== 0) {
+            return reject(new Error('Failed to run fake-consumer'));
+          }
+          resolve();
+        });
+        process.on('error', reject);
+      });
+    };
+
+    return service.start(2345)
+      .then(spawnClient)
+      .then(() => {
+        console.log('SUCCESS');
+        process.exit();
+      })
+      .catch((err) => {
+        console.log('FAILURE', err);
+        process.exit(1);
+      });
+  }
+}
+exports.StateGroup = StateGroup;
+
+class FakeProvider {
+  constructor(opts) {
+    this._process = null;
+    this._opts = opts;
+  }
+
+  start(done) {
+    return new Promise((resolve, reject) => {
+        const manners = spawn(mannersBin, ['fake-provider']);
+        const checkMannersServerIsListening = (data) => {
+          if (data.toString().indexOf('listen') !== -1) {
+            resolve();
+          }
+        };
+
+        manners.stdout.on('data', checkMannersServerIsListening);
+
+        if (this._opts.debug || process.env.MANNERS_DEBUG) {
+          console.log('Debug mode enabled. Outputting Manners server to console.');
+
+          manners.stdout.on('data', (data) => {
+            console.log(data.toString());
+          });
+        }
+
+        this._process = manners;
+      }
+    );
+  }
+
+  stop() {
+    return new Promise((resolve, reject) => {
+      this._process.on('exit', resolve);
+      this._process.kill();
+    });
+  }
+
+  interactions() {
+    return new InteractionGroup({
+      baseUrl: 'http://localhost:1234',
+      consumer: this._opts.consumer,
+      provider: this._opts.provider
+    });
+  }
+}
+exports.FakeProvider = FakeProvider;
+
+class FakeConsumer {
+  constructor(contractPath, providerUrl) {
+    this._cfg = {
+      contractPath: contractPath,
+      providerUrl: providerUrl
+    };
+  }
+
+  states() {
+    return new StateGroup(this._cfg, []);
+  }
+}
+exports.FakeConsumer = FakeConsumer;
